222ae0f04c7184ab9aee802fafd0fb84fb1d7112
Petro Manus <petrmanus@gmail.com>
2020-11-04 23:51:37 +0100

    Provided backend logic for loging procedure. Added corresponding methods in UserServiceImp.

--- src/main/java/com/manus/app/registrationloginspringbootsecuritythymeleaf/service/UserServiceImp.java
+++ src/main/java/com/manus/app/registrationloginspringbootsecuritythymeleaf/service/UserServiceImp.java
@@ -6,11 +6,16 @@ import com.manus.app.registrationloginspringbootsecuritythymeleaf.model.entity.U
 import com.manus.app.registrationloginspringbootsecuritythymeleaf.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImp implements UserService {
@@ -34,7 +39,21 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user==null){
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    //Spring Security expecting authorities
+    //thats why here we converting roles to authorities
+    private List<? extends GrantedAuthority> mapRolesToAuthorities(List<Role> roles){
+
+       return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
     }
 }
